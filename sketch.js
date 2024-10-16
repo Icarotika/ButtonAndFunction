76449a54456848fc6ddb0e0769337eaa0336d671
Icarotika <tikaucaro@gmail.com>
2024-10-16 17:44:19 -0300

    nem te conto meu brother2

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(300, 300)
+}
+
+function draw() {
+}
+
+
+function apertou() {
+    document.getElementById("theImage").style.visibility = "visible";
+  document.getElementById("image").style.visibility = "hidden";
+}
