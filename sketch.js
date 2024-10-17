4eb586da353d4d96bf080fcbb8048c0209f7eeca
Icarotika <tikaucaro@gmail.com>
2024-10-17 08:25:14 -0300

    Atualizar o sketch.js

--- sketch.js
+++ sketch.js
@@ -10,3 +10,10 @@ function apertou() {
     document.getElementById("theImage").style.visibility = "visible";
   document.getElementById("image").style.visibility = "hidden";
 }
+
+
+function aperto() {
+
+document.getElementById("image").style.visibility = "visible";
+  document.getElementById("theImage").style.visibility = "hidden";
+}
